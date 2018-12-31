58cb987d63f7ee954b69d74f74c945d3537e2c66
Justin Hall <hall.justin.d@gmail.com>
2018-12-31 20:28:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let i = 0;
+
 function init() {
   // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || ewhich);
   
-
+  if (key === code[i]) {
+    i++;
+    
+    if(i === code.length) {
+      alert("")
+    }
+  }
 }
