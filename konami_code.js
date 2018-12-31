e1a15e55cbd6bb30f91b4149d4105523225ec170
Justin Hall <hall.justin.d@gmail.com>
2018-12-31 20:41:34 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+
+function init() {
+  
+  let i = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    
+    const key = parseInt(e.which || e.detail);
+
+    if (code[i] === key) {
+      i++;
+
+      if (i === code.length) {
+        alert('Huzzah, nerd');
+
+      }
+    } else {
+      i = 0;
+    }
+  });
 }
+
+init();
