6bf8c4e44ea1b728090181974f8db9833956d88e
Justin Hall <hall.justin.d@gmail.com>
2018-12-31 20:34:04 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ let i = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  document.body.addEventListener('keydown', )
+  document.body.addEventListener('keydown', function(e))
   
   const key = parseInt(e.detail || ewhich);
   
