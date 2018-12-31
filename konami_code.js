715c500d43c8da834436901fd427397d0d1253a1
Justin Hall <hall.justin.d@gmail.com>
2018-12-31 20:36:04 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ let i = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
-  document.body.addEventListener('keydown', function(e){}
+  document.body.addEventListener('keydown', function(e){
   
     const key = parseInt(e.detail || ewhich);
     
@@ -18,5 +18,5 @@ function init() {
     else {
       i = 0;
     }
-  )
+  )}
 }
